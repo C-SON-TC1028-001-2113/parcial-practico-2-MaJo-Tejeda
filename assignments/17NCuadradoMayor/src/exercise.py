55e0287b6ac6a264a37d8b6ed08f9c1ae9a1f6a0
MaJo-Tejeda <88874353+MaJo-Tejeda@users.noreply.github.com>
2021-09-10 10:07:47 -0700

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,15 @@
-
+import math 
 
 def main():
-    numero = int(input("Escribe un numero : "))
-    #escribe tu código abajo de esta línea
 
+ num = int(input("Escribe un numero : "))
+ 
+ ste = math.sqrt(num)
+ 
+ i=1 
+ while i < ste:
+     i = i+1 
+ print (i)
 
 if __name__=='__main__':
     main()
